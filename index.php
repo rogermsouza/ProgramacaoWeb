334672a1f83414014748ceec80359877b0c3f42e
Rogério Maciel de Souza <89948453+rogermsouza@users.noreply.github.com>
2023-08-23 10:36:44 -0300

    página inicial criada

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de exercícios - Programação web</title>
+</head>
+<body>
+    <h2>Lista de Exercícios</h2>
+    <p><a href="lista1.php">Exercício 1</a></p>
+</body>
+</html>
