2b4698a050d253d4756666fd5cd7594a569baca0
rogermsouza <rogermsouza@gmail.com>
2023-09-20 11:04:22 -0300

    finalizando aviao

--- aviao.php
+++ aviao.php
@@ -1,58 +1,87 @@
 <?php
-class Data{
-    private $dia;
-    private $mes;
-    private $ano;
 
-    public function __construct($dia = 0, $mes = 0, $ano = 0)
-    {
-        $this->dia = sprintf("%02s", $dia);
-        $this->mes = sprintf("%02s", $mes);
-        $this->ano = $ano;
-    }
-
-    public function getDia(){
-        return $this->dia;
-    }
-    public function setDia($dia){
-        $this->dia = $dia;
-    }
-    public function maisDia($d){
-        $this->dia += $d;
-    }
-
-    public function menosDia($d){
-        $this->dia -= $d;
-    }
-
-    public function getMes(){
-        return $this->mes;
-    }
-    public function setMes($mes){
-        $this->mes = $mes;
-    }
-
-    public function getAno(){
-        return $this->ano;
-    }
-    public function setAno($ano){ 
-        $this->ano = $ano;
-    }
-
-    public function dataCompleta(){
-        echo "Data: " .$this->dia ."/" .$this->mes  ."/" .$this->ano;
-    }
-    
-    class voo {
-        private $passageiros;
+    class Voo {
         private $num_voo;
-        private $data;
-        public function __construct($passageiros){
-            $this->passageiros = 100;
+        private $data_voo;
+        private $poltrona = array();
+        public function __construct($num_voo, $dia, $mes, $ano){
             $this->num_voo = $num_voo;
-            $this->data = $data;
+            //$this->data = Data($dia, $mes, $ano);
+            $this->data_voo = new DateTime("$ano-$mes-$dia");
+            $this->criaPoltrona(100);
         }
+
+        
+
+
+        public function setNum_voo($num){
+            $this->num_voo = $num;
+        }
+
+        public function getNum_voo(){
+            return $this->num_voo;
+        }
+
+        
+        public function criaPoltrona($num){
+            for($i = 0; $i <100; $i++){
+                $this->poltrona[$i] = 0;
+            }
+        }
+        public function proximaPoltrona(){
+            for($i = 0; $i <100; $i ++){
+                if ($this->poltrona[$i] == 0){
+                    return $i;
+                }
+            }
+        }
+
+        public function verificaPoltrona($num){
+            if($this->poltrona[$num] == 0){
+                return True;
+            }else{
+                return False;
+            }
+        }
+
+        public function ocupaPoltrona($num){
+            $conferePoltrona;
+            if($this->poltrona[$num] == 0){
+                $this->poltrona[$num] = 1;
+                return "Poltrona " .$num ." Reservada";
+            }else{
+                return "Poltrona " .$num ." não disponível <br>";
+            }
+
+        }
+
+        public function getVagas(){
+            $contador = 0;
+            for($i = 0; $i < 100; $i ++){
+                if($this->poltrona[$i] == 0){
+                    $contador += 1;
+                }
+            }
+            return "Total de vagas disponíveis: " .$contador;
+        }
+
+        public function getVoo(){
+            return $this->num_voo;
+        }
+
+        public function getDataVoo($dia, $mes, $ano){
+            return $this->Data();
+        }
+
     }
 
+
+    $voo1 = new Voo(200, 10,05,2023);
+    echo "<br>" .$voo1->ocupaPoltrona(5). "<br>";
+    echo "<br>" .$voo1->ocupaPoltrona(15). "<br>";
+    echo "<br>" .$voo1->ocupaPoltrona(15). "<br>";
+    echo $voo1->getVagas(). "<br>";
+    echo "Número do voo: " .$voo1->getVoo(). "<br>";
+ 
 
 ?>
